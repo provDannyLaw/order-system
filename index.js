5a0ad102e83dffc2166c5b196e92f71783203fdd
dannylaw <danny.law@providence.org>
2021-03-04 15:01:33 -0800

    2nd update

---
+++ index.js
@@ -0,0 +1,9 @@
+import { writeOrder, readOrder } from './lib/orders';
+import { writeCustomer, readCustomer } from "./lib/customers";
+
+export {
+  writeOrder,
+  readOrder,
+  writeCustomer,
+  readCustomer
+};
